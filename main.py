87147b531cfd9e6f89abb686e6126f4969453d87
Granth J. <granth.jain@gmail.com>
2025-08-10 21:43:05 +0100

    Linting

--- main.py
+++ main.py
@@ -34,5 +34,5 @@ def _main() -> None:
     )
     when("click", submit_button, handler=lambda _: _evaluate_solution(code_area.value, output_area, error_area))
 
-
 _main()
+# Issue with Git, so added this line, will remove
