ae139c64d8997f3a42d287ee9b6b53b245f16085
Sebastian Hungerecker <sepp2k@googlemail.com>
2025-08-10 14:08:55 +0200

    Create initial prototype
    This defines a small number of Python functions to create HTML and
    creates the beginnings of a tutorial using them.

---
+++ html_helpers.py
@@ -0,0 +1,51 @@
+from pyscript import document
+from pyscript.web import Element
+
+
+def _tag(tag_name: str, *children: Element | str, **attributes: str) -> Element:
+    node = document.createElement(tag_name)
+    for key, value in enumerate(attributes):
+        node.setAttribute(key, value)
+    for child in children:
+        node.append(child)
+    return node
+
+
+def h1(*children: Element | str, **attributes: str) -> Element:
+    """Create an top level heading."""
+    return _tag("h1", *children, **attributes)
+
+
+def h2(*children: Element | str, **attributes: str) -> Element:
+    """Create a second level heading."""
+    return _tag("h2", *children, **attributes)
+
+
+def div(*children: Element | str, **attributes: str) -> Element:
+    """Create a div element."""
+    return _tag("div", *children, **attributes)
+
+
+def p(*children: Element | str, **attributes: str) -> Element:
+    """Create a paragraph."""
+    return _tag("p", *children, **attributes)
+
+
+def br() -> Element:
+    """Create a line break that does not start a new paragraph."""
+    return _tag("br")
+
+
+def em(*children: Element | str, **attributes: str) -> Element:
+    """Create an em element."""
+    return _tag("em", *children, **attributes)
+
+
+def textarea(*children: Element | str, **attributes: str) -> Element:
+    """Create a text area."""
+    return _tag("textarea", *children, **attributes)
+
+
+def button(*children: Element | str, **attributes: str) -> Element:
+    """Create a button."""
+    return _tag("button", *children, **attributes)
