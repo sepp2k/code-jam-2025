67ef52986ceae7edb1de3e5501433f2eada843ec
Granth J. <granth.jain@gmail.com>
2025-08-10 22:54:08 +0100

    Refactor `_evaluate_solution` for frontend issues, changed functionality to search for elements instead of creating

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import html_helpers
-from html_helpers import br, button, div, em, h1, h2, p, textarea
+from html_helpers import div
 from pyscript import document, when
 from pyscript.web import Element
 
 
-def _evaluate_solution(source: str, output_area: Element, error_area: Element) -> None:
+def _evaluate_solution(source: str = "", output_area: Element = None, error_area: Element = None) -> None:
+    if source.strip() == "" or output_area is None or error_area is None:
+        print("Error, invalid inputs")
+
+    code = str(source)
+
     output_area.innerHTML = ""
     error_area.innerHTML = ""
+
+    is_invalid_html = False
+
+    # Attempt to generate HTML
+    result = None
     try:
-        result = eval(source, globals=html_helpers.__dict__)
-    except Exception as e:  # noqa: BLE001
-        error_area.append(str(e))
+        result = eval(code, globals=html_helpers.__dict__)
+    except Exception:  # noqa: BLE001
+        is_invalid_html = True
+    finally:  # Allow support for input like a plain string (ex. "hello")
+        if result is None:
+            result = str(code)
+
+    # Check if result is a valid HTML element (duck-type)
+    try:
+        print(result.classList)
+    except Exception:  # noqa: BLE001
+        is_invalid_html = True
+
+    if isinstance(result, str):
+        is_invalid_html = False
+
+    if is_invalid_html:
+        error_area.append(div("The code did not produce valid HTML element."))
         return
 
-    # TODO: Before displaying the result we should check that the code actually produced an HTML element and display
-    # a user-readable error message instead of crashing if it did not.
-    output_area.append(result)
     # TODO: Here we should check whether the given HTML fits the required structure and produce an error message
     # if it does not.
 
+    output_area.append(div(result))
+
 
 def _main() -> None:
-    document.body.append(
-        h1("HTML Tutorial"),
-        h2("The em element"),
-        p("The <em> element can be used to emphasized parts of a text ", em("like this"), "."),
-        p("Please create a paragraph (<p> tag) that contains at least two words, exactly one of which is emphasized."),
-        code_area := textarea(),
-        br(),
-        submit_button := button("Submit"),
-        output_area := div(),
-        error_area := div(),
-    )
-    when("click", submit_button, handler=lambda _: _evaluate_solution(code_area.value, output_area, error_area))
+    code_area: Element = Element(document.querySelector("textarea#code-area"))
+    button: Element = Element(document.querySelector("button#submit-button"))
+    output_area: Element = Element(document.querySelector("div#output-area"))
+    error_area: Element = Element(document.querySelector("div#error-area"))
+
+    when("click", button, handler=lambda _: _evaluate_solution(code_area.value, output_area, error_area))
 
 
 _main()
