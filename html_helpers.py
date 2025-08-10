2ad7052930cf77b88760a6e04b91517eb30a099c
Granth J. <granth.jain@gmail.com>
2025-08-10 20:54:24 +0100

    Fixed an issue with attributes for css

--- html_helpers.py
+++ html_helpers.py
@@ -2,31 +2,35 @@ from pyscript import document
 from pyscript.web import Element
 
 
-def _tag(tag_name: str, *children: Element | str, **attributes: str) -> Element:
+def _tag(
+    tag_name: str,
+    *children: Element | str,
+    **attributes: dict[str, str],
+) -> Element:
     node = document.createElement(tag_name)
-    for key, value in enumerate(attributes):
+    for key, value in attributes.items():
         node.setAttribute(key, value)
     for child in children:
         node.append(child)
     return node
 
 
-def h1(*children: Element | str, **attributes: str) -> Element:
+def h1(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create an top level heading."""
     return _tag("h1", *children, **attributes)
 
 
-def h2(*children: Element | str, **attributes: str) -> Element:
+def h2(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create a second level heading."""
     return _tag("h2", *children, **attributes)
 
 
-def div(*children: Element | str, **attributes: str) -> Element:
+def div(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create a div element."""
     return _tag("div", *children, **attributes)
 
 
-def p(*children: Element | str, **attributes: str) -> Element:
+def p(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create a paragraph."""
     return _tag("p", *children, **attributes)
 
@@ -36,16 +40,16 @@ def br() -> Element:
     return _tag("br")
 
 
-def em(*children: Element | str, **attributes: str) -> Element:
+def em(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create an em element."""
     return _tag("em", *children, **attributes)
 
 
-def textarea(*children: Element | str, **attributes: str) -> Element:
+def textarea(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create a text area."""
     return _tag("textarea", *children, **attributes)
 
 
-def button(*children: Element | str, **attributes: str) -> Element:
+def button(*children: Element | str, **attributes: dict[str, str]) -> Element:
     """Create a button."""
     return _tag("button", *children, **attributes)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def _evaluate_solution(source: str, output_area: Element, error_area: Element) -
     output_area.innerHTML = ""
     error_area.innerHTML = ""
     try:
-        result = eval(source, globals=html_helpers.__dict__)
+        result = eval(source, html_helpers.__dict__)
     except Exception as e:  # noqa: BLE001
         error_area.append(str(e))
         return
@@ -24,15 +24,26 @@ def _main() -> None:
     document.body.append(
         h1("HTML Tutorial"),
         h2("The em element"),
-        p("The <em> element can be used to emphasized parts of a text ", em("like this"), "."),
-        p("Please create a paragraph (<p> tag) that contains at least two words, exactly one of which is emphasized."),
+        p(
+            "The <em> element can be used to emphasized parts of a text ",
+            em("like this"),
+            ".",
+        ),
+        p(
+            "Please create a paragraph (<p> tag) that contains at least two words, \
+exactly one of which is emphasized.",
+        ),
         code_area := textarea(),
         br(),
         submit_button := button("Submit"),
         output_area := div(),
         error_area := div(),
     )
-    when("click", submit_button, handler=lambda _: _evaluate_solution(code_area.value, output_area, error_area))
+    when(
+        "click",
+        submit_button,
+        handler=lambda _: _evaluate_solution(code_area.value, output_area, error_area),
+    )
 
 
 _main()
