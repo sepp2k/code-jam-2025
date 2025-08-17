380675739a4330f6b7b863f19c94647abcc9cf32
Sebastian Hungerecker <sepp2k@googlemail.com>
2025-08-17 13:40:05 +0200

    Fix #17: Allow defs, assignments and multipe expressions in user code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ast
 from string import Template
 
 import html_helpers
@@ -62,24 +63,34 @@ def _evaluate_solution(source: str = "", output_area: Element = None, error_area
     is_invalid_html = False
 
     # Attempt to generate HTML
-    result = None
+    output = div()
     err = None
+    environment = {name: value for name, value in html_helpers.__dict__.items() if not name.startswith("_")}
     try:
-        result = eval(source, globals=html_helpers.__dict__)
+        tree = ast.parse(source)
+        for statement in tree.body:
+            match statement:
+                case ast.Expr():
+                    result = eval(compile(ast.Expression(statement.value), "", mode="eval"), globals=environment)
+                    if hasattr(result, "classList") or isinstance(result, str):
+                        output.append(result)
+                    else:
+                        is_invalid_html = True
+                        err = f"""
+                        Expression returned {result} (of type {type(result)}), instead of an HTML element or string
+                        """.strip()
+                case ast.FunctionDef() | ast.Assign():
+                    exec(compile(ast.Module([statement]), "", mode="exec"), globals=environment)
     except Exception as e:  # noqa: BLE001
-        print(f"Exception occurred: {e}")
         is_invalid_html = True
         err = e
 
-    if is_invalid_html or result is None or result == "":
-        print("Invalid HTML")
+    if is_invalid_html:
         error_area.append(div("The code did not produce valid HTML element.", br(), b("Error"), f": {err!s}"))
         _update_iframe(output_area, "")
         return
 
-    # TODO: Before displaying the result we should check that the code actually produced an HTML element and display
-    # a user-readable error message instead of crashing if it did not.
-    _display_result(output_area, result)
+    _display_result(output_area, output)
 
 
 def _main() -> None:
